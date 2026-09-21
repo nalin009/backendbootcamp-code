28442eed0ae5f6589ea3b540b88ebf74f20aa8ee
nalin009 <nalinsahu009@gmail.com>
2026-09-21 23:10:21 +0530

    6. Find First Repeated Character

--- src/main/java/com/backendbootcamp/string/p006_firstrepeatedcharacter/solution/FirstRepeatedCharacter.java
+++ src/main/java/com/backendbootcamp/string/p006_firstrepeatedcharacter/solution/FirstRepeatedCharacter.java
@@ -4,31 +4,31 @@ import java.util.Map;
 import java.util.HashMap;
 
 public class FirstRepeatedCharacter {
-	
-	public static char findRepeatedCharacter(String name){
-	      //sanitize input
-	      name = name.replaceAll("[^a-zA-Z0-9]","").toLowerCase();
 
-	      //Find First Repeated Character
-	      Map<Character, Integer> seen = new HashMap<>();
+	public static char findRepeatedCharacter(String name) {
+		// sanitize input
+		name = name.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
 
-	      for(char ch: name.toCharArray()){
-	        seen.put(ch, seen.getOrDefault(ch,0)+1);
-	      }
+		// Find First Repeated Character
+		Map<Character, Integer> seen = new HashMap<>();
 
-	      for(char ch: name.toCharArray()){
-	        if(seen.get(ch) != 1){
-	          return ch;
-	        }
-	      }
+		for (char ch : name.toCharArray()) {
+			seen.put(ch, seen.getOrDefault(ch, 0) + 1);
+		}
 
-	      return '-';
-	    }
+		for (char ch : name.toCharArray()) {
+			if (seen.get(ch) != 1) {
+				return ch;
+			}
+		}
+
+		return '-';
+	}
 
 	public static void main(String[] args) {
 		String name = "Nalin Sahu";
-	      char ch = findRepeatedCharacter(name);
-	      System.out.println("Repeated Character is : "+ch);
+		char ch = findRepeatedCharacter(name);
+		System.out.println("Repeated Character is : " + ch);
 
 	}
 
