10f45c502de88f852ea14a66d638b92448ae705f
nalin009 <nalinsahu009@gmail.com>
2026-09-21 23:08:16 +0530

    2. Palindrome

--- src/main/java/com/backendbootcamp/string/p002_palindrome/solution/CheckPalindromeWithBuiltInMethod.java
+++ src/main/java/com/backendbootcamp/string/p002_palindrome/solution/CheckPalindromeWithBuiltInMethod.java
@@ -4,13 +4,13 @@ public class CheckPalindromeWithBuiltInMethod {
 
 	public static void main(String[] args) {
 		String name = "madam";
-		
+
 		StringBuilder sb = new StringBuilder(name);
-		
-		if(name.equals(sb.reverse().toString())) {
-			System.out.println(name+" is Palindrome");
-		}else {
-			System.out.println(name+" is not Palindrome");
+
+		if (name.equals(sb.reverse().toString())) {
+			System.out.println(name + " is Palindrome");
+		} else {
+			System.out.println(name + " is not Palindrome");
 		}
 	}
 
--- src/main/java/com/backendbootcamp/string/p002_palindrome/solution/CheckPalindromeWithOutBuiltInMethod.java
+++ src/main/java/com/backendbootcamp/string/p002_palindrome/solution/CheckPalindromeWithOutBuiltInMethod.java
@@ -4,22 +4,22 @@ public class CheckPalindromeWithOutBuiltInMethod {
 
 	public static void main(String[] args) {
 		String name = "madamq";
-		
-		int nameLength = name.length()-1;
+
+		int nameLength = name.length() - 1;
 		int left = 0;
 		boolean isPalindrome = true;
-		
-		while(left < nameLength) {
-			if(name.charAt(left) != name.charAt(nameLength)) {
+
+		while (left < nameLength) {
+			if (name.charAt(left) != name.charAt(nameLength)) {
 				isPalindrome = false;
 				break;
 			}
-			
+
 			left++;
 			nameLength--;
 		}
-		
-		System.out.println("Given String is Palindrome : "+isPalindrome);
+
+		System.out.println("Given String is Palindrome : " + isPalindrome);
 	}
 
 }
