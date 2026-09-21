a46b8d4213061ca9b8cb3ba35cd802944fcaf0bc
nalin009 <nalinsahu009@gmail.com>
2026-09-21 17:44:20 +0530

    Reverse Given String

--- src/main/java/com/backendbootcamp/string/reversestring/solution/ReverseStringUsingMethod.java
+++ src/main/java/com/backendbootcamp/string/reversestring/solution/ReverseStringUsingMethod.java
@@ -3,7 +3,18 @@ package com.backendbootcamp.string.reversestring.solution;
 public class ReverseStringUsingMethod {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String name = "Nalin Sahu 1 ";
+		
+		
+		//sanitize string
+		name = name.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+		
+		
+		//reverse() method
+		String reverse = new StringBuilder(name).reverse().toString();
+		
+		System.out.println("Original : "+name);
+		System.out.println("Reversed : "+reverse);
 
 	}
 
---
+++ src/main/java/com/backendbootcamp/string/reversestring/solution/ReverseStringWithOutUsingMethod.java
@@ -0,0 +1,25 @@
+package com.backendbootcamp.string.reversestring.solution;
+
+public class ReverseStringWithOutUsingMethod {
+
+	public static void main(String[] args) {
+		String name = "1234";
+		
+		//sanitize string
+		name = name.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+		
+		//reverse without using reverse()
+		StringBuilder sb = new StringBuilder();
+		int nameLenght = name.length()-1;
+		
+		
+		for(int i=nameLenght; i>=0; i--) {
+			sb.append(name.charAt(i));
+		}
+		
+		System.out.println("Original : "+name);
+		System.out.println("Reversed : "+sb.toString());
+
+	}
+
+}
